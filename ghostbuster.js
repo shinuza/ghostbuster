c778a256812543626d813185ff2669d2e70a3992
Samori Gorse <sgorse@lefigaro.fr>
2013-10-07 11:35:57 +0200

    Refactoring

--- ghostbuster.js
+++ ghostbuster.js
@@ -2,33 +2,36 @@ var args = require('system').args;
 var fs = require('fs');
 var page = require('webpage').create();
 var interval;
-var clips = [];
-var started;
 var previous;
 
 page.viewportSize = { width: args[2], height: args[3] };
 page.clipRect = { width: args[2], height: args[3] };
 
-page.onLoadStarted = function() {
-  started = +(new Date);
-  interval = setInterval(function() {
-    var imageData = page.renderBase64('png');
-    var clip = {
+var clips = [];
+var started;
+
+function capture() {
+  var imageData = page.renderBase64('png');
+
+  // Skip identical images
+  if(previous != imageData) {
+    clips.push({
       imageData: imageData,
       date: +(new Date)
-    };
+    });
+  }
 
-    // Skip identical images
-    if(previous != imageData) {
-      clips.push(clip);
-    }
+  previous = imageData;
+}
 
-    previous = imageData;
-  }, 100);
+page.onLoadStarted = function() {
+  started = +(new Date);
+  interval = setInterval(capture, 100);
 };
 
 
-page.open(args[1], function(status) {
+page.open(args[1], function() {
+  capture();
   var json = JSON.stringify({
     started: started,
     clips: clips
